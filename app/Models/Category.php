9695dba3bc4faf1b2f4c8e8229a4a44915730830
Pheak02 <saingsopheak02@gmail.com>
2024-06-01 08:51:10 +0700

    has one not many

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -13,6 +13,6 @@ class Category extends Model
     protected $fillable = ['name'];
     public function products()
     {
-        return $this->hasMany(Product::class);
+        return $this->hasOne(Product::class);
     }
 }
