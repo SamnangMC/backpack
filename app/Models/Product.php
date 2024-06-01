75cf664e572a8fb4bfaeef357170dda66e811f4f
Pheak02 <saingsopheak02@gmail.com>
2024-06-01 08:05:44 +0700

    initialize working project + done config

---
+++ app/Models/Product.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use CrudTrait;
+    use HasFactory;
+    protected $fillable = ['name', 'description', 'price', 'quantity'];
+}
